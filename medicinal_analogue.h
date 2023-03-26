1a2ecb45dc2dff2ff4e617d2b409e3d565d0cbc1
Tr0ubad0ur <125650555+Tr0ubad0ur@users.noreply.github.com>
2023-03-26 17:07:01 +0300

    Update medicinal_analogue.h

--- medicinal_analogue.h
+++ medicinal_analogue.h
@@ -1,1 +1,39 @@
+#include <iostream>
+
+using namespase std;
+
+class Medication 
+{  
+  private:
+      string name;
+      double price;
+      string compound;
+  public:
+      void SetInitial(string FileName, double FilePrice, string FileCompound)
+      {
+        name = FileName;
+        price = FilePrice;
+        compound = FileCompound;
+      }
+      
+      void GetInfo()
+      {
+        while (true)
+        {
+          cout << "Введите название препарата (чтобы закончить подбор введите '0')";
+          string name;
+          if (name == 0)
+            break;
+          for (int i = 0; i < (вектор из базы данных позже сделаю).size; ++i)
+          {
+            if ((вектор из базы данных позже сделаю)[i].name)
+            {
+              cout << "Название: " + (вектор из базы данных позже сделаю)[i].name << endl;
+              cout << "Цена:" << (вектор из базы данных позже сделаю)[i].price << endl;
+              cout << "Состав:" + (вектор из базы данных позже сделаю)[i].compound << endl; 
+            }
+        }
+      }
+};
+
 
