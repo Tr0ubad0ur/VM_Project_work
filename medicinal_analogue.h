33dbd5e9c895a8e7e8c73179bb7110f26ccc57e5
Tr0ubad0ur <125650555+Tr0ubad0ur@users.noreply.github.com>
2023-03-26 17:13:03 +0300

    Update medicinal_analogue.h

--- medicinal_analogue.h
+++ medicinal_analogue.h
@@ -16,24 +16,8 @@ class Medication
         compound = FileCompound;
       }
       
-      void GetInfo()
-      {
-        while (true)
-        {
-          cout << "Введите название препарата (чтобы закончить подбор введите '0')";
-          string name;
-          if (name == 0)
-            break;
-          for (int i = 0; i < (вектор из базы данных позже сделаю).size; ++i)
-          {
-            if ((вектор из базы данных позже сделаю)[i].name)
-            {
-              cout << "Название: " + (вектор из базы данных позже сделаю)[i].name << endl;
-              cout << "Цена:" << (вектор из базы данных позже сделаю)[i].price << endl;
-              cout << "Состав:" + (вектор из базы данных позже сделаю)[i].compound << endl; 
-            }
-        }
-      }
+      void GetInfo();
+      
 };
 
 
