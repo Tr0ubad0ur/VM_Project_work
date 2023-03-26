ae235f62f87735671b63542f556841dbb4175989
Tr0ubad0ur <125650555+Tr0ubad0ur@users.noreply.github.com>
2023-03-26 16:37:55 +0300

    Update medicinal_analogue.cpp

--- medicinal_analogue.cpp
+++ medicinal_analogue.cpp
@@ -1,15 +1,3 @@
 #include <medicinal_analodue.h>
 
-class Medication {
-  public:
-      string name;
-      int price;
-      string compound;
-};
 
-class Analogue {
-  public:
-      string name;
-      int price;
-      string compound;
-};
