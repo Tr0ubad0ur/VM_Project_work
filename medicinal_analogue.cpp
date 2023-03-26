ecb599619f22f14cfb06db783f1515d6c424ceac
Tr0ubad0ur <125650555+Tr0ubad0ur@users.noreply.github.com>
2023-03-26 17:10:08 +0300

    Update medicinal_analogue.cpp

--- medicinal_analogue.cpp
+++ medicinal_analogue.cpp
@@ -1,3 +1,20 @@
 #include <medicinal_analodue.h>
 
-
+void medicinal_analodue::GetInfo()
+      {
+        while (true)
+        {
+          cout << "Введите название препарата (чтобы закончить подбор введите '0')";
+          string name;
+          if (name == 0)
+            break;
+          for (int i = 0; i < (вектор из базы данных позже сделаю).size; ++i)
+          {
+            if ((вектор из базы данных позже сделаю)[i].name)
+            {
+              cout << "Название: " + (вектор из базы данных позже сделаю)[i].name << endl;
+              cout << "Цена:" << (вектор из базы данных позже сделаю)[i].price << endl;
+              cout << "Состав:" + (вектор из базы данных позже сделаю)[i].compound << endl; 
+            }
+        }
+      }
